ed908abfa5dd34196ffde9890842941a70a017b6
hectorLop <lopez.almazan.hector@gmail.com>
2021-06-10 19:37:28 +0200

    cleaning_preprocess: Refactored functionalities

--- src/preprocessing/cleaning_preprocesses.py
+++ src/preprocessing/cleaning_preprocesses.py
@@ -1,6 +1,16 @@
-from typing import Dict
+from typing import Dict, List
 import pandas as pd
 import numpy as np
+
+class Pipeline():
+    def __init__(self, preprocesses: List) -> None:
+        self.preprocesses = preprocesses
+
+    def transformm(self, dataframe):
+        for preprocess in self.preprocesses:
+            dataframe = preprocess(dataframe)
+
+        return dataframe
 
 class RemoveSpecialCharacters():
     """
@@ -18,17 +28,19 @@ class RemoveSpecialCharacters():
     _characters : str
         String containing the special characters to be removed
     """
-    def __init__(self, characters: str = '\n* ') -> None:
-        self._characters = characters
+    def __init__(self, columns: List[str], characters: List[str]) -> None:
+        self._characters = ''.join(characters)
+        self._columns = columns
 
-    def __call__(self, dataframe: pd.DataFrame, column: str) -> pd.DataFrame:
-        # Check if the dataframe column is of type string
-        if not dataframe[column].dtype == 'O':
-            raise ValueError('Dataframe column values must be strings')
-        
-        # Remove the special characters
-        dataframe[column] = dataframe[column].apply(
-            self._remove_special_characters)
+    def __call__(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        for column in self._columns:
+            # Check if the dataframe column is of type string
+            if not dataframe[column].dtype == 'O':
+                raise ValueError(f'{column} column must be of type string')
+            
+            # Remove the special characters
+            dataframe[column] = dataframe[column].apply(
+                                                self._remove_special_characters)
 
         return dataframe
 
@@ -66,11 +78,13 @@ class RenameTeams():
     """
     Rename a certain set of teams
     """
-    def __init__(self) -> None:
-        pass
+    def __init__(self, columns: List[str], map_dict: Dict) -> None:
+        self._columns = columns
+        self._map_dict = map_dict
 
-    def __call__(self, dataframe: pd.DataFrame, column: str, map_dict: Dict) -> pd.DataFrame:
-        # Replace the old names by the new ones
-        dataframe[column] = dataframe[column].replace(map_dict)
+    def __call__(self, dataframe: pd.DataFrame) -> pd.DataFrame:
+        for column in self._columns:
+            # Replace the old names by the new ones
+            dataframe[column] = dataframe[column].replace(self._map_dict)
 
         return dataframe
