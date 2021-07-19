33ca2e479e8320f68d31f2ac9c7dfae3cf0c9530
hectorLop <lopez.almazan.hector@gmail.com>
2021-07-19 19:43:59 +0200

    Feature: Implemented logging

---
+++ src/config/logger_config.py
@@ -0,0 +1,55 @@
+import logging
+import logging.config
+import sys
+import os
+
+from pathlib import Path
+
+LOGS_DIR = os.path.join(sys.path[0], 'logs')
+print(LOGS_DIR)
+
+# Logger
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "minimal": {"format": "%(message)s"},
+        "detailed": {
+            "format": "%(levelname)s %(asctime)s [%(filename)s:%(funcName)s:%(lineno)d]\n%(message)s\n"
+        },
+    },
+    "handlers": {
+        "console": {
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+            "formatter": "minimal",
+            "level": logging.DEBUG,
+        },
+        "info": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "info.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.INFO,
+        },
+        "error": {
+            "class": "logging.handlers.RotatingFileHandler",
+            "filename": Path(LOGS_DIR, "error.log"),
+            "maxBytes": 10485760,  # 1 MB
+            "backupCount": 10,
+            "formatter": "detailed",
+            "level": logging.ERROR,
+        },
+    },
+    "loggers": {
+        "root": {
+            "handlers": ["console", "info", "error"],
+            "level": logging.INFO,
+            "propagate": True,
+        },
+    },
+}
+
+logging.config.dictConfig(logging_config)
+logger = logging.getLogger("root")
