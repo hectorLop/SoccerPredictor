05c8025dbc7e279b2467cb4579de3c2e2221dd83
hectorLop <lopez.almazan.hector@gmail.com>
2021-09-28 19:41:57 +0200

    refactor: Changed data fixtures

--- test/data/data_fixtures.py
+++ test/data/data_fixtures.py
@@ -26,32 +26,18 @@ def get_df_to_clean():
 @fixture
 def get_features_df():
     results_dir = os.path.join(THIS_DIR, 'results_1999.csv')
-    results = pd.DataFrame(results_dir)
+    results = pd.read_csv(results_dir)
 
     general_ranking_dir = os.path.join(THIS_DIR, 'general_ranking_1999.csv')
-    general_ranking = pd.DataFrame(general_ranking_dir)
+    general_ranking = pd.read_csv(general_ranking_dir)
 
     home_ranking_dir = os.path.join(THIS_DIR, 'home_ranking_1999.csv')
-    home_ranking = pd.DataFrame(home_ranking_dir)
+    home_ranking = pd.read_csv(home_ranking_dir)
 
     away_ranking_dir = os.path.join(THIS_DIR, 'away_ranking_1999.csv')
-    away_ranking = pd.DataFrame(away_ranking_dir)
+    away_ranking = pd.read_csv(away_ranking_dir)
 
     results_trans_dir = os.path.join(THIS_DIR, 'results_1999_transformed.csv')
-    results_trans = pd.DataFrame(results_trans_dir)
+    results_trans = pd.read_csv(results_trans_dir)
 
     return results, general_ranking, home_ranking, away_ranking, results_trans
-
-# @fixture
-# def get_features_df():
-#     matches_df = pd.read_csv(os.path.join(THIS_DIR,
-#                                         'test_matches_df_features_pip.csv'))
-
-#     return matches_df
-
-# @fixture
-# def get_transformed_test_data():
-#     test_data = np.load(os.path.join(THIS_DIR,
-#                                         'transformed_test_data.npy'))
-
-#     return test_data
