7a2b16af1513cab9a0b9b19d73b47a849ed533ef
hectorLop <lopez.almazan.hector@gmail.com>
2021-09-28 19:42:47 +0200

    refactor: Refactored preprocesses unitary testing

--- test/preprocessing/test_feature_preprocessing.py
+++ test/preprocessing/test_feature_preprocessing.py
@@ -2,50 +2,99 @@ from test.data.data_fixtures import get_features_df
 import pytest
 
 from src.preprocessing.features_preprocesses import (
-    ComputeGoalsConceded, ComputeGoalsScored,
-    ComputeHomeWins, ComputeAwayWins,
-    ComputeHomeDraws, ComputeAwayDraws,
-    ComputeHomeLosses, ComputeAwayLosses,
-    ComputeDrawsStreak, ComputeLossesStreak,
-    ComputeWinsStreak, ComputeLeagueRankPosition,
-    FeaturePipeline
+    ComputeLeagueRankPosition,
+    ComputeWins,
+    ComputeDraws,
+    ComputeLosses,
+    ComputeGoalsConceded,
+    ComputeGoalsScored,
+    FeaturePipeline,
 )
 from pandas._testing import assert_frame_equal
 
-from test.data.data_fixtures import get_features_df, get_cleaned_df
+from test.data.data_fixtures import get_features_df
 
-def test_feature_pipeline(get_cleaned_df, get_features_df):
-    matches_df, rank_df = get_cleaned_df
-    expected_matches_df = get_features_df
-    columns = ['team_1', 'team_2']
+def test_feature_pipeline(get_features_df):
+    results, general, home, away, expected_results = get_features_df
 
-    pipeline = FeaturePipeline([
-        ComputeGoalsConceded(rank_df, columns,
-                        ['goals_conceded_t1', 'goals_conceded_t2']),
-        ComputeGoalsScored(rank_df, columns,
-                        ['goals_scored_t1', 'goals_scored_t2']),
-        ComputeHomeWins(rank_df, columns,
-                        ['home_wins_t1', 'home_wins_t2']),
-        ComputeAwayWins(rank_df, columns,
-                        ['away_wins_t1', 'away_wins_t2']),
-        ComputeHomeLosses(rank_df, columns,
-                        ['home_losses_t1', 'home_losses_t2']),
-        ComputeAwayLosses(rank_df, columns,
-                        ['away_losses_t1', 'away_losses_t2']),
-        ComputeHomeDraws(rank_df, columns,
+    # Compute general, home and away wins
+    wins_pipeline = FeaturePipeline([
+        ComputeWins(general,
+                    ['team_1', 'team_2'],
+                    ['general_wins_t1', 'general_wins_t2']),
+        ComputeWins(home,
+                    ['team_1', 'team_2'],
+                    ['home_wins_t1', 'home_wins_t2']),
+        ComputeWins(away,
+                    ['team_1', 'team_2'],
+                    ['away_wins_t1', 'away_wins_t2']),
+    ])
+    # General, home and away draws
+    draws_pipeline = FeaturePipeline([
+        ComputeDraws(general,
+                    ['team_1', 'team_2'],
+                    ['general_draws_t1', 'general_draws_t2']),
+        ComputeDraws(home,
+                    ['team_1', 'team_2'],
                         ['home_draws_t1', 'home_draws_t2']),
-        ComputeAwayDraws(rank_df, columns,
-                        ['away_draws_t1', 'away_draws_t2']),
-        ComputeLeagueRankPosition(rank_df, columns,
-                        ['rank_position_t1', 'rank_position_t2']),
-        ComputeWinsStreak(rank_df, columns,
-                        ['wins_streak_t1', 'wins_streak_t2']),
-        ComputeDrawsStreak(rank_df, columns,
-                        ['draws_streak_t1', 'draws_streak_t2']),
-        ComputeLossesStreak(rank_df, columns,
-                        ['losses_streak_t1', 'losses_streak_t2'])
+        ComputeDraws(away,
+                    ['team_1', 'team_2'],
+                    ['away_draws_t1', 'away_draws_t2']),
+    ])
+    # General, home and away losses
+    loss_pipeline = FeaturePipeline([
+        ComputeLosses(general,
+                    ['team_1', 'team_2'],
+                    ['general_losses_t1', 'general_losses_t2']),
+        ComputeLosses(home,
+                    ['team_1', 'team_2'],
+                    ['home_losses_t1', 'home_losses_t2']),
+        ComputeLosses(away,
+                    ['team_1', 'team_2'],
+                    ['away_losses_t1', 'away_losses_t2']),      
+    ])
+    # General, home and away goals scored
+    goals_scored_pipeline = FeaturePipeline([
+        ComputeGoalsScored(
+                general,
+                ['team_1', 'team_2'],
+                ['general_goals_scored_t1', 'general_goals_scored_t2']),
+        ComputeGoalsScored(
+                home,
+                ['team_1', 'team_2'],
+                ['home_goals_scored_t1', 'home_goals_scored_t2']),
+        ComputeGoalsScored(
+                away,
+                ['team_1', 'team_2'],
+                ['away_goals_scored_t1', 'away_goals_scored_t2'])
+    ])
+    # General, home and away goals conceded
+    goals_conceded_pipeline = FeaturePipeline([
+        ComputeGoalsConceded(
+                general,
+                ['team_1', 'team_2'],
+                ['general_goals_conceded_t1', 'general_goals_conceded_t2']),
+        ComputeGoalsConceded(
+                home,
+                ['team_1', 'team_2'],
+                ['home_goals_conceded_t1', 'home_goals_conceded_t2']),
+        ComputeGoalsConceded(
+                away,
+                ['team_1', 'team_2'],
+                ['away_goals_conceded_t1', 'away_goals_conceded_t2'])
     ])
 
-    matches_df_trans = pipeline.transform(matches_df)
+    # Feature preprocessing pipeline
+    pipeline = FeaturePipeline([
+        ComputeLeagueRankPosition(general, ['team_1', 'team_2'],
+                                ['rank_t1', 'rank_t2']),
+        wins_pipeline,
+        draws_pipeline,
+        loss_pipeline,
+        goals_scored_pipeline,
+        goals_conceded_pipeline
+    ])
 
-    assert_frame_equal(matches_df_trans, expected_matches_df)
+    results_trans = pipeline(results)
+
+    assert_frame_equal(results_trans, expected_results)
