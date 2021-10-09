aabb1166769d0e119d5153c9cd57b9f201b7e18d
hectorLop <lopez.almazan.hector@gmail.com>
2021-10-09 10:16:31 +0200

    feature: Containearized web app

--- src/web_app/web_app.py
+++ src/web_app/web_app.py
@@ -1,30 +1,44 @@
 import streamlit as st
 import pandas as pd
-import requests
 import json
 import boto3
 import os
 
 from dotenv import load_dotenv
-from src.config.config import DATA_DIR
 
 load_dotenv()
-bucket = os.getenv('AWS_BUCKET')
 
-s3 = boto3.client('s3')
-result = s3.get_object(Bucket=bucket, Key='predictions/predictions.json')
-data = result["Body"].read()
-data = json.loads(data)
+def get_data():
+    bucket = os.getenv('AWS_BUCKET')
+    s3 = boto3.client('s3')
 
-league_match = data["league_match"]
+    result = s3.get_object(Bucket=bucket, Key='predictions/predictions.json')
+
+    data = result["Body"].read()
+    data = json.loads(data)
+
+    return data
+
 # Writing App Title and Description
-st.title(f'Spanish LaLiga Prediction - League Match {league_match}')
+st.title(f'Spanish LaLiga Prediction')
 st.write('This is a web app to predict the outcome of LaLiga soccer matches.')
 
-for i in range(10):
-    st.markdown(f'## Match {i+1}')
-    team_1 = data[f'match_{i}']['team_1']
-    team_2 = data[f'match_{i}']['team_2']
-    outcome = data[f'match_{i}']['outcome']
+if st.button('Get Results'):
+    data = get_data()
+    league_match = data["league_match"]
 
-    st.write(f'{team_1} vs {team_2} ----> {outcome}')
+    st.subheader(f'League Match {league_match}')
+
+    df_data = []
+
+    for i in range(10):
+        #st.markdown(f'## Match {i+1}')
+        team_1 = data[f'match_{i}']['team_1']
+        team_2 = data[f'match_{i}']['team_2']
+        outcome = data[f'match_{i}']['outcome']
+
+        df_data.append((team_1, team_2, outcome))
+
+    df = pd.DataFrame(df_data, columns=['team_1', 'team_2', 'outcome'])
+
+    st.dataframe(df)
