75c555e2319f2bd18c626163cbd2869b436287c1
hectorLop <lopez.almazan.hector@gmail.com>
2021-11-03 19:45:42 +0100

    refactor: Changed features

--- src/preprocessing/config.py
+++ src/preprocessing/config.py
@@ -5,7 +5,7 @@ RANKING_COLS = ['season', 'league_match', 'rank_pos', 'team',
                 'goals_conceded', 'goals_difference']
 
 FEATURES_TO_DROP = [
-    'season', 'team_1', 'team_2', 'league_match', 'home_wins_t1', 'home_wins_t2',
+    'season', 'team_1', 'team_2', 'league_match', 'home', 'home_wins_t1', 'home_wins_t2',
     'away_wins_t1', 'away_wins_t2', 'general_wins_t1', 'general_wins_t2',
     'home_draws_t1', 'home_draws_t2', 'away_draws_t1', 'away_draws_t2', 
     'general_draws_t1', 'general_draws_t2', 'home_losses_t1', 'home_losses_t2',
